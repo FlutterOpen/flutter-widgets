f3f027e50893117ef1654547ded2128aa283187f
nb312 <nbfly01@163.com>
2019-05-25 09:14:59 +0800

    add Table and Wrap widget.

--- lib/page/muti/FlowPage.dart
+++ lib/page/muti/FlowPage.dart
@@ -12,17 +12,11 @@ class FlowPage extends StatefulWidget {
 }
 
 class FlowState extends State<FlowPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(PageName.FLOW),
-      ),
-      body: Flow(
+  Widget _flow() => Flow(
         delegate: MFlowDelegate(),
         children: <Widget>[
           Container(
-            constraints: BoxConstraints.expand(height: 100),
+            constraints: BoxConstraints.expand(height: 100, width: 200),
             color: RED,
           ),
           Container(
@@ -31,8 +25,110 @@ class FlowState extends State<FlowPage> {
             color: BLUE,
           )
         ],
-      ),
-    );
+      );
+
+  ///  this.direction = Axis.horizontal,
+//    this.alignment = WrapAlignment.start,
+//    this.spacing = 0.0,
+//    this.runAlignment = WrapAlignment.start,
+//    this.runSpacing = 0.0,
+//    this.crossAxisAlignment = WrapCrossAlignment.start,
+//    this.textDirection,
+//    this.verticalDirection = VerticalDirection.down,
+
+  Widget _wrap() => Wrap(
+        spacing: 8.0,
+        // gap between adjacent chips
+        runSpacing: 10.0,
+        // gap between lines
+        alignment: WrapAlignment.spaceAround,
+        direction: Axis.horizontal,
+        runAlignment: WrapAlignment.spaceEvenly,
+        crossAxisAlignment: WrapCrossAlignment.center,
+        verticalDirection: VerticalDirection.down,
+        children: <Widget>[
+          Chip(
+            avatar: CircleAvatar(
+                backgroundColor: Colors.blue.shade900, child: Text('AH')),
+            label: Text('Hamilton'),
+          ),
+          Chip(
+            avatar: CircleAvatar(
+                backgroundColor: Colors.blue.shade900, child: Text('ML')),
+            label: Text('Lafayette'),
+          ),
+          Chip(
+            avatar: CircleAvatar(
+                backgroundColor: Colors.blue.shade900, child: Text('HM')),
+            label: Text('Mulligan'),
+          ),
+          Chip(
+            avatar: CircleAvatar(
+                backgroundColor: Colors.blue.shade900, child: Text('JL')),
+            label: Text('Laurens'),
+          ),
+        ],
+      );
+
+//    this.children = const <TableRow>[],
+//    this.columnWidths,
+//    this.defaultColumnWidth = const FlexColumnWidth(1.0),
+//    this.textDirection,
+//    this.border,
+//    this.defaultVerticalAlignment = TableCellVerticalAlignment.top,
+//    this.textBaseline,
+  Widget _table() => Table(
+        border: TableBorder.all(color: RED, width: 1, style: BorderStyle.solid),
+        defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+        children: [
+          TableRow(children: [
+            Container(
+              constraints: BoxConstraints.expand(height: 100),
+              alignment: Alignment.center,
+              child: Text(
+                "HHHH",
+                textAlign: TextAlign.center,
+              ),
+            ),
+            Text("HHHH"),
+            Text("HHHH"),
+            Text("HHHH"),
+            Text("HHHH"),
+          ]),
+          TableRow(children: [
+            Text("HHHH"),
+            Text("HHHH"),
+            Text("HHHH"),
+            Text("HHHH"),
+            Text("HHHH"),
+          ]),
+        ],
+      );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(PageName.FLOW),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Container(
+                constraints: BoxConstraints.expand(height: 400),
+                child: _flow(),
+              ),
+              Container(
+                constraints: BoxConstraints.expand(height: 100),
+                child: _wrap(),
+              ),
+              Container(
+                constraints: BoxConstraints.expand(height: 400),
+                child: _table(),
+              )
+            ],
+          ),
+        ));
   }
 }
 
